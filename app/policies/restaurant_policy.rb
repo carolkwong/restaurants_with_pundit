4099123a55fefbeecafe1fed4144a17bf1b87d85
Carol Kwong <carol.kwong@gmail.com>
2019-12-04 11:57:44 +0800

    added admin field to user, updated restaurant policy

--- app/policies/restaurant_policy.rb
+++ app/policies/restaurant_policy.rb
@@ -26,16 +26,19 @@ class RestaurantPolicy < ApplicationPolicy
     #user == current_user
 
     # return restaurant.user == current_user
-    record.user == user
+    user_is_owner_or_admin?
   end
 
   def update?
-    record.user == user
+    user_is_owner_or_admin?
   end
 
   def destroy?
-    record.user == user
+    user_is_owner_or_admin?
   end
 
-
+  private
+    def user_is_owner_or_admin?
+      record.user == user || user.admin
+    end
 end
